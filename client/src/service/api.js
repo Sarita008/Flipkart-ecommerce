67a49c96f140ca8e422660b71b75d6d386afa553
Sarita008 <saritasharma8201@gmail.com>
2024-12-10 04:36:33 +0530

    Code

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://flipkart-ecommerce.onrender.com';
+const url = 'http://localhost:8000';
 
 export const authenticateLogin = async (user) => {
     try {
